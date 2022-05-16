5db956801432008b3b1d491ef33e353aadca70e1
Zhenia <zhenia1985@gmail.com>
2022-05-17 01:55:30 +0700

    реализована прокрутка табов

--- src/components/BurgerIngredients/BurgerIngredients.jsx
+++ src/components/BurgerIngredients/BurgerIngredients.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react"
-import {Ingridients} from "./Ingridients"
+import { Ingridients } from "./Ingridients"
 import ingredientsStyles from "./BurgerIngredients.module.css"
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components"
 import { ingredientsPropTypes } from "../../utils/types"
 import PropTypes from "prop-types"
 
-export const BurgerIngredients = ({data, onOpen}) => {
+export const BurgerIngredients = ({ data, onOpen }) => {
 
-   const [current, setCurrent] = useState("one")
+   const [current, setCurrent] = useState("bun")
 
    const buns = data.filter((item) => {
       return item.type === "bun"
@@ -19,20 +19,26 @@ export const BurgerIngredients = ({data, onOpen}) => {
       return item.type === "main"
    })
 
+   const handleClick = (id) => {
+      setCurrent(id)
+      const anchor = document.getElementById(id)
+      anchor.scrollIntoView({ behavior: "smooth" })
+   }
+
    return (
       <section className={ingredientsStyles.section}>
          <h1 className={ingredientsStyles.title}>Соберите бургер</h1>
          <nav className={ingredientsStyles.nav}>
-            <Tab value="one" active={current === "one"} onClick={setCurrent}>Булки</Tab>
-            <Tab value="two" active={current === "two"} onClick={setCurrent}>Соусы</Tab>
-            <Tab value="three" active={current === "three"} onClick={setCurrent}>Начинки</Tab>
+            <Tab value="one" active={current === "one"} onClick={() => handleClick("one")}>Булки</Tab>
+            <Tab value="two" active={current === "two"} onClick={() => handleClick("two")}>Соусы</Tab>
+            <Tab value="three" active={current === "three"} onClick={() => handleClick("three")}>Начинки</Tab>
          </nav>
          <article className={ingredientsStyles.article}>
-            <h2 className={ingredientsStyles.subtitle}>Булки</h2>
-            <Ingridients ingridients={buns} onOpen={onOpen}/>
-            <h2 className={ingredientsStyles.subtitle}>Соусы</h2>
+            <h2 className={ingredientsStyles.subtitle} id="one">Булки</h2>
+            <Ingridients ingridients={buns} onOpen={onOpen} />
+            <h2 className={ingredientsStyles.subtitle} id="two">Соусы</h2>
             <Ingridients ingridients={sauces} onOpen={onOpen} />
-            <h2 className={ingredientsStyles.subtitle}>Начинки</h2>
+            <h2 className={ingredientsStyles.subtitle} id="three">Начинки</h2>
             <Ingridients ingridients={fillings} onOpen={onOpen} />
          </article>
       </section>
