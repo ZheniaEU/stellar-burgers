df79900e3a0f1550efcfba474b247ee4fb763a17
Zhenia <zhenia1985@gmail.com>
2022-07-20 02:57:55 +0700

    test

--- src/services/actions/auth.js
+++ src/services/actions/auth.js
@@ -88,36 +88,39 @@ export const аuthenticationUser = () => {
                      type: LOGIN_USER,
                      user: res.user
                   })
-               } else {
-                  console.log("b")
-                  if (res.message === "jwt expired") {
-                     console.log("c")
-                     refreshToken(getCookie("refreshToken"))
-                        .then(res => {
-                           console.log("d")
-                           if (res.success) {
-                              setCookie("accessToken", res.accessToken.split("Bearer ")[1])
-                              setCookie("refreshToken", res.refreshToken)
-                           }
-                        })
-                     console.log("e")
-                     getUser(getCookie("accessToken"))
-                        .then(res => {
-                           console.log("f")
-                           if (res.success) {
-                              console.log("g")
-                              dispatch({
-                                 type: LOGIN_USER,
-                                 user: res.user
-                              })
-                           }
-                        })
-                  }
                }
             })
-         //     })
-         //вроде по идеи если токен протух его нужно отрефрешить стоит ли его тут рефрешить?
+            .catch(res => {
+               console.log("b")
+               if (res.message === "jwt expired") {
+                  console.log("c")
+                  refreshToken(getCookie("refreshToken"))
+                     .then(res => {
+                        console.log("d")
+                        if (res.success) {
+                           setCookie("accessToken", res.accessToken.split("Bearer ")[1])
+                           setCookie("refreshToken", res.refreshToken)
+                        }
+                     })
+                  console.log("e")
+                  getUser(getCookie("accessToken"))
+                     .then(res => {
+                        console.log("f")
+                        if (res.success) {
+                           console.log("g")
+                           dispatch({
+                              type: LOGIN_USER,
+                              user: res.user
+                           })
+                        }
+                     })
+               }
+
+            })
+
       }
+      //     })
+      //вроде по идеи если токен протух его нужно отрефрешить стоит ли его тут рефрешить?
    }
 }
 
